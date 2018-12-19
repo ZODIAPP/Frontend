25ffee81e78f2bcb77cc9ea15d5866454546b124
Nolan Rudolph <nolanrudolph@Nolans-MacBook-Air.local>
2018-12-19 01:09:02 -0800

    Got Project Setup + Edited Login Page + Beginning Cocoapod Tomorrow

--- Zodi/ViewControllers/SignUpController.swift
+++ Zodi/ViewControllers/SignUpController.swift
@@ -13,14 +13,58 @@ class SignUpController: UIViewController {
     @IBOutlet weak var nameField: UITextField!
     @IBOutlet weak var emailField: UITextField!
     @IBOutlet weak var birthdateField: UITextField!
+    @IBOutlet weak var submitBtn: UIButton!
+    @IBOutlet weak var blankFieldError: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        blankFieldError.isHidden = true
         // Do any additional setup after loading the view.
     }
-    func blankChecker() {
+    
+    
+    @IBAction func submitBtn_pressed(_ sender: Any) {
+        let check = blankChecker()
+        let name = check.0
+        let email = check.1
+        let birthdate = check.2
+        
+        if !name || !email || !birthdate {
+            blankFieldError.isHidden = false
+            let red = UIColor.red
+            if !name {
+                nameField.layer.borderColor = red.cgColor
+                nameField.layer.borderWidth = 1.0
+            }
+            if !email {
+                emailField.layer.borderColor = red.cgColor
+                emailField.layer.borderWidth = 1.0
+
+            }
+            if !birthdate {
+                birthdateField.layer.borderColor = red.cgColor
+                birthdateField.layer.borderWidth = 1.0
+            }
+        }
+        
+    }
+    
+    func blankChecker() -> (name: Bool, email: Bool, birthdate: Bool){
         //This will be used in the loginHandler to error check whether or not the user has given input or not so as to assure correct requests to the API
+        var name = true
+        var email = true
+        var birthdate = true
+        if nameField.text == "" {
+            name = false
+        }
+        if emailField.text == "" {
+            email = false
+        }
+        if birthdateField.text == "" {
+            birthdate = false
+        }
+        
+        return (name, email, birthdate)
     }
     
     func validChecker() {
